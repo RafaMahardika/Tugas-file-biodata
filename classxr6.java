4b782791b54074068d725e17102caa362e93ba28
RafaMahardika <cllowiioyt@gmail.com>
2025-08-05 08:40:22 +0700

    Add files via upload

---
+++ classxr6.java
@@ -0,0 +1,30 @@
+public class classxr6 {
+    public static void main(String[] args) {
+       //membuat variabel
+       String nama, alamat, asalsekolah, jeniskelamin, hobi, citacita;
+         int umur;
+         double tinggi;
+        
+        //mengisi variabel
+        nama = "Rafa";
+        alamat = "jl renang";
+        hobi = "Basket";
+        citacita = "Backend Developer";
+        umur = 15;
+        tinggi = 100.5;
+        asalsekolah = "SMPK Telkom Malang";
+        jeniskelamin = "Laki-laki";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Asal Sekolah: " + asalsekolah);
+        System.out.println("Jenis Kelamin: " + jeniskelamin);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Hobi: " + hobi);
+        System.out.println("Cita-cita: " + citacita);
+
+        
+    }
+    
+}
